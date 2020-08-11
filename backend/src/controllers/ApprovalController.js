5329a8123b52b23453805ce5ee9b8c4644af096b
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2020-08-12 01:11:16 +0530

    Setup frontend

---
+++ backend/src/controllers/ApprovalController.js
@@ -0,0 +1,21 @@
+const registration = require('../models/Registration');
+const Registration = require('../models/Registration');
+
+module.exports = {
+    async approval (req, res) {
+        const { registrationId } = req.params;
+
+        try {
+            const registration = await Registration.findById(registrationId);
+            registration.approved = true;
+            await registration.save();
+    
+            return res.json(registration);
+
+        } catch (error) {
+            return res.status(400).json({
+                message: `Approval failed - ${error}`
+            });
+        }
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const multer = require('multer');
 
+const ApprovalController = require('./controllers/ApprovalController');
 const DashboardController = require('./controllers/DashboardController');
 const EventController = require('./controllers/EventController');
 const LoginController = require('./controllers/LoginController');
 const RegistrationController = require('./controllers/RegistrationController');
+const RejectionController = require('./controllers/RejectionController');
 const UserController = require('./controllers/UserController');
 const uploadConfig = require('./config/upload');
 
@@ -14,11 +16,11 @@ const upload = multer(uploadConfig);
 
 routes.get('/', (request, response) => {
     response.send('hello from express');
+    
+    
+    //Dashboard
+    routes.get('/dashboard/:sport', DashboardController.getAllEvents);
 });
-
-
-//Dashboard
-routes.get('/dashboard/:sport', DashboardController.getAllEvents);
 routes.get('/dashboard', DashboardController.getAllEvents);
 routes.get('/event/:eventId', DashboardController.getEventById);
 
@@ -32,6 +34,11 @@ routes.post('/login', LoginController.store);
 //Registration
 routes.post('/registration/:eventId', RegistrationController.create);
 routes.get('/registration/:registrationId', RegistrationController.getRegistration);
+
+//Approvals
+routes.post('/registration/:registrationId/approval', ApprovalController.approval);
+//Rejections
+routes.post('/registration/:registrationId/rejection', RejectionController.rejection);
 
 //Users
 routes.post('/user/register', UserController.createUser);
